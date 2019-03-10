761a94c82d64edde3ff3182e7124c09bce603b9e
John Barker <jebarker@gmail.com>
2019-03-10 16:02:56 -0600

    Serve up static files from static/ dir with actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate actix_web;
+use actix_web::{server, App, http::Method, http::header, fs, HttpResponse};
+
 fn main() {
-    println!("Hello, world!");
+    server::new(
+        || App::new()
+            .handler("/s", fs::StaticFiles::new("static").unwrap())
+            .resource("/", |r| r.method(Method::GET).f(|req| {
+                println!("{:?}", req);
+                HttpResponse::Found()
+                    .header(header::LOCATION, "/s/index.html")
+                    .finish()
+        }))
+        )
+        .bind("127.0.0.1:8080").unwrap()
+        .run();
 }
