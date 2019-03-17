35bd27173f1e91eea4f64bcc81ffb1dcbe590719
John Barker <jebarker@gmail.com>
2019-03-17 16:20:27 -0600

    If profile is not debug then don't keep debug symbols via bindgen

--- build.rs
+++ build.rs
@@ -1,18 +1,35 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
     let out_dir = "static";
+    let profile = env::var("PROFILE").unwrap();
 
-    Command::new("wasm-bindgen")
-        .args(&["./viewer/target/wasm32-unknown-unknown/debug/viewer.wasm",
-              "--out-dir", out_dir,
-              "--no-typescript",
-              "--browser",
-              "--no-modules"])
-        .args(&["--keep-debug", "--debug", "--no-demangle"])
-        .status().unwrap();
+    let source_wasm = format!("viewer/target/wasm32-unknown-unknown/{}/viewer.wasm", profile).to_string();
+
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}", source_wasm);
+    println!("cargo:rerun-if-changed=viewer/src/*.rs");
+    println!("cargo:rerun-if-env-changed=PROFILE");
+
+    let core_bindgen_args = [
+        &source_wasm,
+        "--out-dir", out_dir,
+        "--no-typescript",
+        "--browser",
+        "--no-modules"];
+    if profile == "debug" {
+        Command::new("wasm-bindgen")
+            .args(&core_bindgen_args)
+            .args(&["--keep-debug", "--debug", "--no-demangle"])
+            .status().unwrap();
+    } else {
+        Command::new("wasm-bindgen")
+            .args(&core_bindgen_args)
+            .status().unwrap();
+    }
 
     Command::new("wasm-gc")
-        .args(&["./static/viewer_bg.wasm"])
+        .args(&["static/viewer_bg.wasm"])
         .status().unwrap();
 }
