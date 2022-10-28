9eb8f5b9f9f4e572a888f862619cd30b50912e8e
John Barker <dev@j16r.net>
2022-10-28 15:38:52 +0800

    Add scales/axes, adjust to window size

--- viewer/src/lib.rs
+++ viewer/src/lib.rs
@@ -6,7 +6,8 @@ use bevy::{
     render::camera::Camera,
     render::mesh::Mesh,
     render::render_resource::PrimitiveTopology,
-    sprite::MaterialMesh2dBundle,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+    window::WindowResized,
 };
 use tracing::trace;
 
@@ -35,8 +36,9 @@ pub fn main() {
         .insert_resource(DrumBeat(Timer::from_seconds(1.0, true)))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(update_mouse_motion)
+        // .add_system(update_mouse_motion)
         .add_system(clock)
+        .add_system(on_resize)
         .run();
 
     trace!("start up done");
@@ -57,19 +59,99 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn().insert_bundle(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(LineGraph {
-            points: vec![
-                Vec3::ZERO,
-                Vec3::new(100.0, 100.0, 0.0),
-                Vec3::new(100.0, 0.0, 0.0),
-                Vec3::new(0.0, 100.0, 0.0),
-            ],
-        })).into(),
+        mesh: meshes
+            .add(Mesh::from(LineGraph {
+                points: vec![
+                    Vec3::ZERO,
+                    Vec3::new(100.0, 100.0, 0.0),
+                    Vec3::new(100.0, 0.0, 0.0),
+                    Vec3::new(0.0, 100.0, 0.0),
+                ],
+            }))
+            .into(),
         material: materials.add(Color::BLUE.into()),
         ..default()
     });
 
+    let axes = Axes::new();
+    let mesh_bundle = MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(&axes)).into(),
+        material: materials.add(Color::BLACK.into()),
+        ..default()
+    };
+    commands
+        .spawn()
+        .insert(axes)
+        .insert(mesh_bundle.mesh.0.clone())
+        .insert_bundle(mesh_bundle);
+
     commands.spawn_bundle(Camera2dBundle::default());
+}
+
+#[derive(Debug, Clone)]
+pub struct Scale {
+    pub label: String,
+    pub min: f32,
+    pub max: f32,
+}
+
+#[derive(Debug, Component, Clone)]
+pub struct Axes {
+    x: Scale,
+    y: Scale,
+    view_size: Size,
+}
+
+impl Axes {
+    fn new() -> Self {
+        Axes {
+            x: Scale {
+                label: "???".to_string(),
+                min: 0.0,
+                max: 100.0,
+            },
+            y: Scale {
+                label: "???".to_string(),
+                min: 0.0,
+                max: 100.0,
+            },
+            view_size: Size::default(),
+        }
+    }
+}
+
+impl From<&Axes> for Mesh {
+    fn from(this: &Axes) -> Self {
+        let mut vertices = vec![];
+        let mut normals = vec![];
+        let mut uvs = vec![];
+
+        let padding = this.view_size.width * 0.1;
+        let min_x = (this.view_size.width - padding) / 2.0;
+        let min_y = (this.view_size.height - padding) / 2.0;
+
+        vertices.push([-min_x, min_y, 0.0]);
+        vertices.push([-min_x, -min_y, 0.0]);
+        vertices.push([min_x, -min_y, 0.0]);
+        normals.push(Vec3::ZERO.to_array());
+        uvs.push([0.0; 2]);
+        normals.push(Vec3::ZERO.to_array());
+        uvs.push([0.0; 2]);
+        normals.push(Vec3::ZERO.to_array());
+        uvs.push([0.0; 2]);
+
+        // This tells wgpu that the positions are a list of points
+        // where a line will be drawn between each consecutive point
+        let mut mesh = Mesh::new(PrimitiveTopology::LineStrip);
+
+        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+
+        // Normals are currently required by bevy, but they aren't used by the [`LineMaterial`]
+        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+
+        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+        mesh
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -118,4 +200,18 @@ fn update_mouse_motion(
     info!("camera = {:?}", camera);
 
     camera.translation += Vec3::new(1.0, 0.0, 0.0);
+}
+
+fn on_resize(
+    mut resize_reader: EventReader<WindowResized>,
+    mut axes: Query<(&mut Axes, &Handle<Mesh>)>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let (mut axes, handle) = axes.get_single_mut().unwrap();
+    for e in resize_reader.iter() {
+        axes.view_size.width = e.width;
+        axes.view_size.height = e.height;
+        info!("resize {:.1} x {:.1}", e.width, e.height);
+        meshes.set(handle, Mesh::from(&*axes).into());
+    }
 }
